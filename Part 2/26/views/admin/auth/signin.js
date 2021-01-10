4776d288d2f2b22b8e4ae08d97f23ab08159db46
Matias Duhalde <matias.duhalde@uc.cl>
2021-01-10 10:09:35 -0300

    :art: Add style and expose public resources

--- Part 2/26/views/admin/auth/signin.js
+++ Part 2/26/views/admin/auth/signin.js
@@ -4,14 +4,26 @@ const { getError } = require('../../helpers');
 module.exports = ({ errors }) => {
     return layout({
         content: `
-            <div>
-                <form method="POST">
-                    <input name="email" placeholder="email" />
-                    ${getError(errors, 'email')}
-                    <input name="password" placeholder="password" type="password" />
-                    ${getError(errors, 'password')}
-                    <button>Sign In</button>
-                </form>
+            <div class="container">
+                <div class="columns is-centered">
+                    <div class="column is-one-quarter">
+                        <form method="POST">
+                            <h1 class="title">Sign in</h1>
+                            <div class="field">
+                                <label class="label">Email</label>
+                                <input required class="input" placeholder="Email" name="email" />
+                                <p class="help is-danger">${getError(errors, 'email')}</p>
+                            </div>
+                            <div class="field">
+                                <label class="label">Password</label>
+                                <input required class="input" placeholder="Password" name="password" type="password" />
+                                <p class="help is-danger">${getError(errors, 'password')}</p>
+                            </div>
+                            <button class="button is-primary">Submit</button>
+                        </form>
+                        <a href="/signup">Need an account? Sign Up</a>
+                    </div>
+                </div>
             </div>
         `
     });
--- Part 2/26/views/admin/auth/signup.js
+++ Part 2/26/views/admin/auth/signup.js
@@ -1,20 +1,38 @@
 const layout = require('../layout');
 const { getError } = require('../../helpers');
 
-module.exports = ({ req, errors }) => {
+module.exports = ({ errors }) => {
     return layout({
         content: `
-            <div>
+        <div class="container">
+            <div class="columns is-centered">
+                <div class="column is-one-quarter">
                 <form method="POST">
-                    <input name="email" placeholder="email" />
-                    ${getError(errors, 'email')}
-                    <input name="password" placeholder="password" type="password" />
-                    ${getError(errors, 'password')}
-                    <input name="passwordConfirmation" placeholder="password confirmation" type="password" />
-                    ${getError(errors, 'passwordConfirmation')}
-                    <button>Sign Up</button>
+                    <h1 class="title">Sign Up</h1>
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <input required class="input" placeholder="Email" name="email" />
+                        <p class="help is-danger">${getError(errors, 'email')}</p>
+                    </div>
+                    <div class="field">
+                        <label class="label">Password</label>
+                        <input required class="input" placeholder="Password" name="password" type="password" />
+                        <p class="help is-danger">${getError(errors, 'password')}</p>
+                    </div>
+                    <div class="field">
+                        <label class="label">Password Confirmation</label>
+                        <input required class="input" placeholder="Password Confirmation" name="passwordConfirmation" type="password" />
+                        <p class="help is-danger">${getError(
+                            errors,
+                            'passwordConfirmation'
+                        )}</p>
+                    </div>
+                    <button class="button is-primary">Submit</button>
                 </form>
+                <a href="/signin">Have an account? Sign In</a>
+                </div>
             </div>
+        </div>
         `
     });
 }
