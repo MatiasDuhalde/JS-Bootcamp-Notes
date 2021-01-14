bfa7a07330be4c1422f0d0ca191f71d023f34c17
Matias Duhalde <matias.duhalde@uc.cl>
2021-01-14 23:46:56 +0000

    :boom: Add basic cart functionalities and improved style

---
+++ Part 2/26/repositories/carts.js
@@ -0,0 +1,7 @@
+const Repository = require('./repository');
+
+class CartsRepository extends Repository {
+
+}
+
+module.exports = new CartsRepository('carts.json');
---
+++ Part 2/26/routes/carts.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
+
+const router = express.Router();
+
+router.get('/cart', async (req, res) => {
+    if (!req.session.cartId) {
+        return res.redirect('/');
+    }
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    for (let item of cart.items) {
+        const product = await productsRepo.getOne(item.id);
+        item.product = product;
+    }
+
+    res.send(cartShowTemplate({ items: cart.items }));
+});
+
+router.post('/cart/products', async (req, res) => {
+    let cart;
+    if (!req.session.cartId) {
+        // Create new cart and store in req.session.cartId
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+    const existingItem = cart.items.find(item => item.id === req.body.productId);
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.items.push({
+            id: req.body.productId,
+            quantity: 1
+        })
+    }
+
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    });
+
+    res.redirect('/cart');
+});
+
+router.post('/cart/products/delete', async (req, res) => {
+    let cart;
+    if (!req.session.cartId) {
+        return
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+
+    const { itemId } = req.body;
+    const items = cart.items.filter(item => item.id !== itemId);
+
+    await cartsRepo.update(cart.id, { items });
+
+    res.redirect('/cart');
+});
+
+
+module.exports = router;
